40efbd22f736c64632ae76a1dac7884c96e9db67
ekg98 <ekg98@indy.rr.com>
2021-01-03 11:20:31 -0500

    Fixed out of bound card value and suit.  This should fix phantom table error and blank card errors.

--- cards.c
+++ cards.c
@@ -1,42 +1,53 @@
-/* Functions for dealing with the handling of the cards in video poker */
+// Functions for dealing with the handling of the cards in video poker
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include "cards.h"
 
-/* deal:  This function takes a structure of cards as input and randomly populates the deck with new cards.  This checks to see if the cards are held and if they are duplicates too. */
+// deal:  This function takes a structure of cards as input and randomly populates the deck with new cards.  This checks to see if the cards are held and if they are duplicates too.
 void deal(struct card *hand, int size)
 {
-	int i, dupcheck, duplicate = NO;
-
-	for(i = 0; i < size; i++)
+	int intCounter, dupcheck;
+	bool duplicate = false;
+	
+	for(intCounter = 0; intCounter < size; intCounter++)
 	{
-		/* if card is not held generate a new card on function call */
-		if(hand[i].held == NO)
+		// if card is not held generate a new card on function call
+		if(hand[intCounter].held == NO)
 		{
-			hand[i].value = 2 + (rand() / (RAND_MAX / 13));
+			// generate cards
+			hand[intCounter].value = 2 + (rand() / (RAND_MAX / 13));
+			hand[intCounter].suit = 1 + (rand() / (RAND_MAX / 4));
 
-			hand[i].suit = 1 + (rand() / (RAND_MAX / 4));
+			// check for out of bounds value
+			while(hand[intCounter].value < 2 || hand[intCounter].value >= 15)
+				hand[intCounter].value = 2 + (rand() / (RAND_MAX / 13));
+						
+			// check for out of bounds suit
+			while(hand[intCounter].suit < 1 || hand[intCounter].suit >= 5)
+				hand[intCounter].suit = 1 + (rand() / (RAND_MAX / 4));
 		}
 	}
 
-	/* check for duplicate cards */
-	for(i = 0; i < size; i++)
+	// cycle through the hand one card at a time.  This is the card that is being checked against the other cards
+	for(intCounter = 0; intCounter < size; intCounter++)
 	{
+		// cycle through the hand checking the other cards against intCounter card flag them as duplicates
 		for(dupcheck = 0; dupcheck < size; dupcheck++)
 		{
-			if(dupcheck != i)
-				if(hand[i].value == hand[dupcheck].value && hand[i].suit == hand[dupcheck].suit)
+			// Do not check card being checked
+			if(dupcheck != intCounter)
+				if(hand[intCounter].value == hand[dupcheck].value && hand[intCounter].suit == hand[dupcheck].suit)
 				{
-					duplicate = YES;
+					duplicate = true;
 					break;
 				}
-
 		}
 	}
 
-	if(duplicate == YES)
+	if(duplicate == true)
 	{
 		deal(hand, size);
 	}
